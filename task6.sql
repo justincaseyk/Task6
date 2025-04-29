83caab3ba267443a11fb3ed739132e88bfab05e1
Abhishek Gudesaria <abhishek.goodaysiryeah@gmail.com>
2025-04-29 20:38:01 +0530

    Add files via upload

---
+++ task6.sql
@@ -0,0 +1,8 @@
+use arise;
+SELECT * from task6;
+select id, name from task6;
+select sum(revenue) from task6 group by region;
+select count(distinct id) from task6;
+select unitsold, date from task6 order by date desc;
+SELECT * FROM task6 WHERE date BETWEEN '1/1/2024' AND '8/27/2024' ORDER BY date;
+SELECT name, revenue FROM task6 ORDER BY revenue DESC;
